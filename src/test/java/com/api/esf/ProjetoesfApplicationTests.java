6e26cb4ebef599981a3a3d26c6cc79e2a57de8f1
elionaifigueiredo <elionaifigueiredo@gmail.com>
2023-08-28 19:33:37 -0400

    desc projeto

---
+++ src/test/java/com/api/esf/ProjetoesfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.esf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoesfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
